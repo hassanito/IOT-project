f483d65f8a3ef2329044a53a27bcbff0783a41a6
hassanito <1Aa_12345>
2020-01-07 07:06:18 +0200

    final version with working mqtt

--- iotproject1/mqtt.py
+++ iotproject1/mqtt.py
@@ -8,7 +8,7 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     #previously #
-    client.subscribe("gateway/7276ff000b03008c/event/number")
+    client.subscribe("application/19/device/6452f0b72169118d/rx")
 
 # The callback for when a PUBLISH message is received from the server.s
 def on_message(client, userdata, msg):
@@ -18,10 +18,15 @@ def on_message(client, userdata, msg):
     messages.append(msg.payload)
     j = json.loads(msg.payload)
     #overhere I have to update this with the working topic and data content in json -test with the actual device
+    state = 0
+    if(j['applicationID']=='19'):
+        if(j["deviceName"]=="IoT-G3"):
+            state = j['object']['payload']
+            print('STATE = '+state)
 
-    professor_name = j['Professor']
+
+    professor_name = "Samer Lahoud"
     pr = Professor.objects.get(professor=professor_name)
-    state = j['State']
     print("previous state is "+str(state))
     print("previous in office is "+str(pr.in_office))
     #when a message is received we check if the previous state saved in the database and the state from the bottom are different which means the
@@ -29,7 +34,7 @@ def on_message(client, userdata, msg):
     #state =1 implies professor.in_office = True
     #state =0 implies professor.in_office = False
 
-    if(pr.in_office == False and state ==1):
+    if(pr.in_office == False and state =="1"):
         pr.in_office =True
         pr.save()
         print("SEND MAILS")
@@ -41,7 +46,7 @@ def on_message(client, userdata, msg):
             #print(current_student.student.email)
             send_presence_mail(i.get_time,current_student,pr)
 
-    elif(pr.in_office == True and state ==0):
+    elif(pr.in_office == True and state =="0"):
         pr.in_office =False
         pr.save()
         #goes over all the subscribers of this professor and sends an email of his current stat that changed
